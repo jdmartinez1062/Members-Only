ce387959da250d251b6747451e54385532a67a11
jdmartinez1062 <jd.martinez1062@gmail.com>
2020-03-26 20:31:38 -0500

    fixed errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   include SessionsHelper
+  before_action :remember_create, only: [:create]
   def new
     @user= User.new()
   end
@@ -27,6 +28,10 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  def remember_create
+    @user= User.new()
+    @user.remember
   end
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,17 +2,16 @@ module SessionsHelper
     
 
     def log_in(user)
-        session[:user_id] = user.id
-        remember(user)
+        user.remember
+        cookies.signed[:user_id] = user.id
+        cookies[:remember_token] = user.remember_token
     end
     
     def current_user 
-        
-        if  (user_id = session[:user_id])
+        if (user_id = cookies.signed[:user_id])
             @current_user ||= User.find_by(id: user_id)
-        elsif (user_id = cookies.signed[:user_id])
-            @current_user ||= User.find_by(id: user_id)
-            log_in(@current_user)
+            log_in(@current_user) if @current_user
+            @current_user
         end
     end
 
@@ -23,13 +22,7 @@ module SessionsHelper
     def logged_in?
         !current_user.nil?
     end
-    def remember(user)
-        cookies.signed[:user_id] = user.id
-        cookies[:remember_token] = user.remember_token
-        user.remember
-    end
     def log_out(user)
-        session.delete(:user_id)
         user.forget
         cookies.delete(:user_id)
         cookies.delete(:remember_token)
